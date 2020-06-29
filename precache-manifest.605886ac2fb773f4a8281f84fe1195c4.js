d976c8301e08bbf457cb2baa5b7999bcf8a2523e
changheon.kim <changheon.kim@purplelabs.co.kr>
2020-06-29 13:11:49 +0900

    Updates

---
+++ precache-manifest.605886ac2fb773f4a8281f84fe1195c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d196f1620a7fbf0d0aca3dc9184cf1",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "fabda0073adf4858e5a2",
+    "url": "/movie_app/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fabda0073adf4858e5a2",
+    "url": "/movie_app/static/js/main.814ce8c8.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
